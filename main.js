6625ff5ad0f4643cea0bf581c522c6e7a9c1090e
TrentHurlbut <trent.hurlbut@gmail.com>
2021-10-20 09:27:15 -0700

    error handling

--- main.js
+++ main.js
@@ -14,38 +14,43 @@ document.getElementById('discovery-form').addEventListener(
         e.preventDefault();
 
         let artistText = document.getElementById('discovery-text').value;
-
-        fetch(
-            `https://itunes.apple.com/search?media=music&entity=song&term=${artistText}`
-        )
-            .then((response) => response.json())
-            .then((data) => {
-                let musicArr = data.results;
-                if (resultsField.innerHTML === '') {
-                    for (let i = 0; i < 16; i++) {
-                        let selector = Math.floor(Math.random() * 50);
-                        resultsField.innerHTML += `
-                    <div class="artist card">
-                    <img src=${musicArr[selector].artworkUrl100}>
-                    <p class='song-info'>${musicArr[selector].artistName} </br> ${musicArr[selector].trackName}</p>
-                    <button class='play' id='${musicArr[selector].trackId}' value='${musicArr[selector].previewUrl}'>Play Me!</button>
-                    </div>
-                    `;
+        try {
+            fetch(
+                `https://itunes.apple.com/search?media=music&entity=song&term=${artistText}`
+            )
+                .then((response) =>
+                    response.ok ? response.json() : alert(response.statusText)
+                )
+                .then((data) => {
+                    let musicArr = data.results;
+                    if (resultsField.innerHTML === '') {
+                        for (let i = 0; i < 16; i++) {
+                            let selector = Math.floor(Math.random() * 50);
+                            resultsField.innerHTML += `
+                        <div class="artist card">
+                        <img src=${musicArr[selector].artworkUrl100}>
+                        <p class='song-info'>${musicArr[selector].artistName} </br> ${musicArr[selector].collectionName} </br> ${musicArr[selector].trackName}</p>
+                        <button class='play' id='${musicArr[selector].trackId}' value='${musicArr[selector].previewUrl}'>Play Me!</button>
+                        </div>
+                        `;
+                        }
+                    } else {
+                        resultsField.innerHTML = '';
+                        for (let i = 0; i < 16; i++) {
+                            let selector = Math.floor(Math.random() * 50);
+                            resultsField.innerHTML += `
+                        <div class="artist card">
+                        <img src=${musicArr[selector].artworkUrl100}>
+                        <p class='song-info'>${musicArr[selector].artistName} </br> ${musicArr[selector].trackName}</p>
+                        <button class='play' id='${musicArr[selector].trackId}' value='${musicArr[selector].previewUrl}'>Play Me!</button>
+                        </div>
+                        `;
+                        }
                     }
-                } else {
-                    resultsField.innerHTML = '';
-                    for (let i = 0; i < 16; i++) {
-                        let selector = Math.floor(Math.random() * 50);
-                        resultsField.innerHTML += `
-                    <div class="artist card">
-                    <img src=${musicArr[selector].artworkUrl100}>
-                    <p class='song-info'>${musicArr[selector].artistName} </br> ${musicArr[selector].trackName}</p>
-                    <button class='play' id='${musicArr[selector].trackId}' value='${musicArr[selector].previewUrl}'>Play Me!</button>
-                    </div>
-                    `;
-                    }
-                }
-            });
+                });
+        } catch (error) {
+            alert("Your search didn't work! Try again.");
+        }
     },
     false
 );
